5e3752e01836adc568d0e793f0f449dd04fcff98
JNyaga <joelnyagamuriithi@gmail.com>
2025-01-14 10:36:59 +0300

    193: Add Database struct for connection pooling and Update sqlx dependency to version 0.8

--- src/lib/data/mod.rs
+++ src/lib/data/mod.rs
@@ -1,6 +1,6 @@
 use derive_more::{Display, From};
 use serde::{Deserialize, Serialize};
-use sqlx::{Database, Sqlite};
+use sqlx::Sqlite;
 use std::str::FromStr;
 use thiserror::Error;
 use uuid::Uuid;
@@ -16,6 +16,28 @@ pub type DatabasePool = sqlx::sqlite::SqlitePool;
 pub type Transaction<'t> = sqlx::Transaction<'t, Sqlite>;
 pub type AppDatabaseRow = sqlx::sqlite::SqliteRow;
 pub type AppQueryResult = sqlx::sqlite::SqliteQueryResult;
+
+//this struct is used to hold the database connection pool
+pub struct Database<D: sqlx::Database>(sqlx::Pool<D>);
+
+impl Database<Sqlite> {
+    pub async fn new(connection_str: &str) -> Self {
+        let pool = sqlx::sqlite::SqlitePoolOptions::new()
+            .connect(connection_str)
+            .await;
+        match pool {
+            Ok(pool) => Self(pool),
+            Err(e) => {
+                eprintln!("{}\n", e);
+                eprintln!("If the database has not been created run: \n $sqlx database setup\n");
+                panic!("Database connection error");
+            }
+        }
+    }
+    pub fn get_pool(&self) -> &DatabasePool {
+        &self.0
+    }
+}
 
 #[derive(Clone, Debug, Deserialize, Serialize, Display, From)]
 pub struct Dbid(Uuid);
